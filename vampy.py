000f1e3fc28cdc2304da5238904ab23f5665f81c
vampysecurity <56776434+TuxUnix@users.noreply.github.com>
2020-09-04 17:15:52 +0000

    Add files via upload

---
+++ vampy.py
@@ -0,0 +1,22 @@
+#coded by vampy
+import sys, os, webbrowser, platform, subprocess
+
+import webbrowser
+
+def __limpiar():
+	if os.name == 'nt':
+            os.system('cls')
+	else:
+            os.system('clear')
+def menu():
+	__limpiar()
+
+print ("   ____   ____                                    ")
+print ("  |_  _| |_  _|                                  ")
+print ("    \ \   / /,--.   _ .--..--.  _ .--.   _   __    ")
+print ("     \ \ / /`'_\ : [ `.-. .-. |[ '/'`\ \[ \ [  ] ")
+print ("      \ ' / // | |, | | | | | | | \__/ | \ '/ /  ")
+print ("       \_/  \'-;__/[___||__||__]| ;.__/[\_:  /    ")
+print ("                               [__|     \__.'      ")
+print (" este script va a ser la verga en forma de clavo ")
+
